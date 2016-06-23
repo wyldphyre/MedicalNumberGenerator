78eda7911f8bd4a92591addb4432406cc5d3e687
Craig Reynolds <wyldphyre@gmail.com>
2016-06-23 10:50:44 +0800

    Code clean up

--- Medical Number Generator/MaskedTextRandomValueGenerators.cs
+++ Medical Number Generator/MaskedTextRandomValueGenerators.cs
@@ -1,60 +1,18 @@
 using System;
-using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace MedicalNumberGenerator
 {
   class MaskedTextRandomValueGenerator
   {
-    private List<char> alphanumericCharacterList = new List<char>();
-    private List<char> alphabeticCharacterList = new List<char>();
-    private List<char> numericCharacterList = new List<char>();
+    private readonly char[] alphanumericCharacters;
+    private readonly char[] alphabeticCharacters = new[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+    private readonly char[] numericCharacters = new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
 
     public MaskedTextRandomValueGenerator()
     {
-      alphabeticCharacterList.Add('A');
-      alphabeticCharacterList.Add('B');
-      alphabeticCharacterList.Add('C');
-      alphabeticCharacterList.Add('D');
-      alphabeticCharacterList.Add('E');
-      alphabeticCharacterList.Add('F');
-      alphabeticCharacterList.Add('G');
-      alphabeticCharacterList.Add('H');
-      alphabeticCharacterList.Add('I');
-      alphabeticCharacterList.Add('J');
-      alphabeticCharacterList.Add('K');
-      alphabeticCharacterList.Add('L');
-      alphabeticCharacterList.Add('M');
-      alphabeticCharacterList.Add('N');
-      alphabeticCharacterList.Add('O');
-      alphabeticCharacterList.Add('P');
-      alphabeticCharacterList.Add('Q');
-      alphabeticCharacterList.Add('R');
-      alphabeticCharacterList.Add('S');
-      alphabeticCharacterList.Add('T');
-      alphabeticCharacterList.Add('U');
-      alphabeticCharacterList.Add('V');
-      alphabeticCharacterList.Add('W');
-      alphabeticCharacterList.Add('X');
-      alphabeticCharacterList.Add('Y');
-      alphabeticCharacterList.Add('Z');
-
-      numericCharacterList.Add('1');
-      numericCharacterList.Add('2');
-      numericCharacterList.Add('3');
-      numericCharacterList.Add('4');
-      numericCharacterList.Add('5');
-      numericCharacterList.Add('6');
-      numericCharacterList.Add('7');
-      numericCharacterList.Add('8');
-      numericCharacterList.Add('9');
-      numericCharacterList.Add('0');
-
-      foreach (char character in alphabeticCharacterList)
-        alphanumericCharacterList.Add(character);
-
-      foreach (char character in numericCharacterList)
-        alphanumericCharacterList.Add(character);
+      alphanumericCharacters = alphabeticCharacters.Union(numericCharacters).ToArray();
     }
 
     public void Execute()
@@ -73,13 +31,13 @@ namespace MedicalNumberGenerator
         switch (maskCharacter)
         {
           case 'A':
-            textBuilder.Append(alphanumericCharacterList[rnd.Next(alphanumericCharacterList.Count)]);
+            textBuilder.Append(alphanumericCharacters[rnd.Next(alphanumericCharacters.Length)]);
             break;
           case 'L':
-            textBuilder.Append(alphabeticCharacterList[rnd.Next(alphabeticCharacterList.Count)]);
+            textBuilder.Append(alphabeticCharacters[rnd.Next(alphabeticCharacters.Length)]);
             break;
           case '9':
-            textBuilder.Append(numericCharacterList[rnd.Next(numericCharacterList.Count)]);
+            textBuilder.Append(numericCharacters[rnd.Next(numericCharacters.Length)]);
             break;
           default:
             textBuilder.Append(maskCharacter);
